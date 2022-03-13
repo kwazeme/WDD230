d78a5695b4e1da01975670559214ac49f22615a6
kwazeme <ogu21006@byui.edu>
2022-03-13 21:25:24 +0200

    contact page done

---
+++ chamber/js/contact.js
@@ -0,0 +1,13 @@
+// Form submission message
+let subForm = document.getElementsByTagName("form")[0];
+subForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    subForm.style.display = "none";
+    document.querySelector(".subMessage").innerHTML = 'Thanks for your message. We will get back to you within 24 hours.'
+    document.querySelector(".sub").innerHTML = `<hr>We Got You<hr><br>`
+
+});
+
+
+
+
